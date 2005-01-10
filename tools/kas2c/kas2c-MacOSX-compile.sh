a5e18f30d9e79aff4b3273ef702a157440e4b4e3
lmop <lmop@026c9d8a-83c9-0310-a9c7-971d0a006279>
2005-01-10 23:44:36 +0000

    Shell script to compile kas2c under Mac OS X.
    git-svn-id: svn://www.homeworldsdl.org:3692/homeworldsdl/homeworld/trunk@74 026c9d8a-83c9-0310-a9c7-971d0a006279

---
+++ tools/kas2c/kas2c-MacOSX-compile.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+# This compiles kas2c on Mac OS X. It could be done as an XCode project
+# but that seems to be a bit of an overkill. The only Mac OS X-specific
+# parts of this script are the #defines (-D) provided to gcc which
+# toggle some header paths.
+
+/bin/echo -----------------------------------------------------------------
+/bin/echo gcc will complain about TRUE/FALSE being redefined when compiling
+/bin/echo kas2c. This is normal and can be safely ignored.
+/bin/echo -----------------------------------------------------------------
+
+/usr/bin/lex -i -olexer.c lexer.l
+/usr/bin/yacc -d -o parser.c parser.y
+/usr/bin/gcc lexer.c parser.c parser.h kas2c.c kas2c.h -o kas2c -D_MACOSX -D_KAS2C
