e865d654974fb4357904b40d82e9b0e384e85cce
lmop <lmop@026c9d8a-83c9-0310-a9c7-971d0a006279>
2010-09-05 16:57:44 +0000

    Clean up of #defines pending additional commits
    git-svn-id: svn://www.homeworldsdl.org:3692/homeworldsdl/homeworld/trunk@726 026c9d8a-83c9-0310-a9c7-971d0a006279

--- src/Game/FormationDefs.h
+++ src/Game/FormationDefs.h
@@ -1,16 +1,22 @@
 #ifndef ___FORMATIONDEFS_H
 #define ___FORMATIONDEFS_H
 
-#define DELTA_FORMATION     0
-#define BROAD_FORMATION     1
-#define DELTA3D_FORMATION   2
-#define CLAW_FORMATION      3
-#define WALL_FORMATION      4
-#define SPHERE_FORMATION    5
+// -----------------------------------------------------------------------------
+// preserve order: index into formationtypeinfo (Formation.c)
+
+#define DELTA_FORMATION     0    // F5
+#define BROAD_FORMATION     1    // F6
+#define DELTA3D_FORMATION   2    // F7
+#define CLAW_FORMATION      3    // F8
+#define WALL_FORMATION      4    // F9
+#define SPHERE_FORMATION    5    // F10
+
 #define PICKET_FORMATION    6
-//#define NO_FORMATION    (SPHERE_FORMATION + 1)
-#define CUSTOM_FORMATION    6
-#define NO_FORMATION    (CUSTOM_FORMATION + 1)
-#define SAME_FORMATION  (NO_FORMATION + 1)          //for computer player
+
+#define CUSTOM_FORMATION    6    // F11
+#define NO_FORMATION        7
+#define SAME_FORMATION      8    // for computer player
+
+// -----------------------------------------------------------------------------
 
 #endif
