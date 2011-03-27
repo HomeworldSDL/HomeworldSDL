5608bb1887f85c676387d34ae220c9bb8cf46f82
lmop <lmop@026c9d8a-83c9-0310-a9c7-971d0a006279>
2011-03-27 16:06:09 +0000

    Removing function prototype for non-existent function: lgFailedToConnectToChannel()
    git-svn-id: svn://www.homeworldsdl.org:3692/homeworldsdl/homeworld/trunk@879 026c9d8a-83c9-0310-a9c7-971d0a006279

--- src/Game/MultiplayerLANGame.h
+++ src/Game/MultiplayerLANGame.h
@@ -188,8 +188,6 @@ lgqueuegameplayerinfo;
 void lgStartMultiPlayerLANGameScreens(regionhandle region, sdword ID, udword event, udword data, bool AlreadyLoggedIn);
 void lgShutdownMultiPlayerGameScreens(void);
 
-void lgFailedToConnectToChannel(bool existing);
-
 void lgDisplayMessage(char *message);
 
 void lgProcessGameChatPacket(struct ChatPacket *packet);
