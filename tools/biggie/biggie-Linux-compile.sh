09591aba4e2c0b4ffa303f74efacff465e8e6415
Thibault Lemaire <thibault.lemaire@protonmail.com>
2019-02-12 12:43:07 +0100

    Reintroduce Linux folder when building biggie
    I'm sorry heckendorfc, but I don't see why I would recompile
    files that will be compiled for the game anyway.

--- tools/biggie/biggie-Linux-compile.sh
+++ tools/biggie/biggie-Linux-compile.sh
@@ -3,5 +3,5 @@ cc -o biggie -Wall -O2 -D_LINUX_FIX_ME -I../../src/ThirdParty/CRC \
         -I../../src/SDL \
 	-I../../src/ThirdParty/LZSS `sdl-config --cflags` \
 	main.c options.c ../../src/Game/BigFile.c \
-	../../src/ThirdParty/CRC/libhw_CRC.a \
-	../../src/ThirdParty/LZSS/libhw_LZSS.a `sdl-config --libs`
+	../../Linux/src/ThirdParty/CRC/libhw_CRC.a \
+	../../Linux/src/ThirdParty/LZSS/libhw_LZSS.a `sdl-config --libs`
