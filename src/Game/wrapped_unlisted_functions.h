0a9c9616efd8c4b0fe604869b2a33cc8b6052ba8
Thibault Lemaire <thibault.lemaire@protonmail.com>
2019-03-07 20:55:52 +0100

    Fix return type of wrapped unlisted functions

--- src/Game/wrapped_unlisted_functions.h
+++ src/Game/wrapped_unlisted_functions.h
@@ -1,17 +1,15 @@
 #define get_arg(n) do_get_arg(effect, stat, opcode, n)
 
-#define memsize udword
-
 void partSetTrHandle(trhandle t, udword u0, udword v0, udword u1, udword v1);
 
-udword wrap_partSetTrHandle(Effect *effect, struct etgeffectstatic *stat ,etgfunctioncall *opcode)
+memsize wrap_partSetTrHandle(Effect *effect, struct etgeffectstatic *stat ,etgfunctioncall *opcode)
 {
 	udword var0 = do_get_arg(effect, stat, opcode, 0);
 	udword var1 = do_get_arg(effect, stat, opcode, 1);
 	udword var2 = do_get_arg(effect, stat, opcode, 2);
 	udword var3 = do_get_arg(effect, stat, opcode, 3);
 	udword var4 = do_get_arg(effect, stat, opcode, 4);
-	udword (*func_ptr)(udword, udword, udword, udword, udword) = (udword (*)(udword, udword, udword, udword, udword)) opcode->function;
+	memsize (*func_ptr)(udword, udword, udword, udword, udword) = (memsize (*)(udword, udword, udword, udword, udword)) opcode->function;
 
 	return func_ptr(var0, var1, var2, var3, var4);
 }
@@ -19,7 +17,7 @@ udword wrap_partSetTrHandle(Effect *effect, struct etgeffectstatic *stat ,etgfun
 
 void partModifyTexture(psysPtr psys, trhandle tex, udword u0, udword v0, udword u1, udword v1);
 
-udword wrap_partModifyTexture(Effect *effect, struct etgeffectstatic *stat ,etgfunctioncall *opcode)
+memsize wrap_partModifyTexture(Effect *effect, struct etgeffectstatic *stat ,etgfunctioncall *opcode)
 {
 	udword var0 = do_get_arg(effect, stat, opcode, 0);
 	udword var1 = do_get_arg(effect, stat, opcode, 1);
@@ -27,27 +25,27 @@ udword wrap_partModifyTexture(Effect *effect, struct etgeffectstatic *stat ,etgf
 	udword var3 = do_get_arg(effect, stat, opcode, 3);
 	udword var4 = do_get_arg(effect, stat, opcode, 4);
 	udword var5 = do_get_arg(effect, stat, opcode, 5);
-	udword (*func_ptr)(udword, udword, udword, udword, udword, udword) = (udword (*)(udword, udword, udword, udword, udword, udword)) opcode->function;
+	memsize (*func_ptr)(udword, udword, udword, udword, udword, udword) = (memsize (*)(udword, udword, udword, udword, udword, udword)) opcode->function;
 
 	return func_ptr(var0, var1, var2, var3, var4, var5);
 }
 
 void partSetMeshdata(meshdata *m);
 
-udword wrap_partSetMeshdata(Effect *effect, struct etgeffectstatic *stat ,etgfunctioncall *opcode)
+memsize wrap_partSetMeshdata(Effect *effect, struct etgeffectstatic *stat ,etgfunctioncall *opcode)
 {
 	udword var0 = do_get_arg(effect, stat, opcode, 0);
-	udword (*func_ptr)(udword) = (udword (*)(udword)) opcode->function;
+	memsize (*func_ptr)(udword) = (memsize (*)(udword)) opcode->function;
 
 	return func_ptr(var0);
 }
 
 void etgCreateCallbackSetup(Effect *effect, sdword codeOffset);
 
-udword wrap_etgCreateCallbackSetup(Effect *effect, struct etgeffectstatic *stat ,etgfunctioncall *opcode)
+memsize wrap_etgCreateCallbackSetup(Effect *effect, struct etgeffectstatic *stat ,etgfunctioncall *opcode)
 {
 	udword var0 = do_get_arg(effect, stat, opcode, 0);
-	udword (*func_ptr)(Effect *, udword) = (udword (*)(Effect *, udword)) opcode->function;
+	memsize (*func_ptr)(Effect *, udword) = (memsize (*)(Effect *, udword)) opcode->function;
 
 	return func_ptr(effect, var0);
 }
