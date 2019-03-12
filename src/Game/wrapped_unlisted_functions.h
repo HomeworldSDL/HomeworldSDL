ffc531e3d1e95e7907df5127cb1647c6d6e80984
Thibault Lemaire <thibault.lemaire@protonmail.com>
2019-03-12 10:01:17 +0100

    Format wrapped_unlisted_functions.h
    I chose to indent with 4 spaces.
    4 is pretty standard, and any decent modern IDE supports indenting with
    spaces.

--- src/Game/wrapped_unlisted_functions.h
+++ src/Game/wrapped_unlisted_functions.h
@@ -2,71 +2,69 @@
 
 void partSetTrHandle(trhandle t, udword u0, udword v0, udword u1, udword v1);
 
-memsize wrap_partSetTrHandle(Effect *effect, struct etgeffectstatic *stat ,etgfunctioncall *opcode)
+memsize wrap_partSetTrHandle(Effect *effect, struct etgeffectstatic *stat, etgfunctioncall *opcode)
 {
-	udword var0 = do_get_arg(effect, stat, opcode, 0);
-	udword var1 = do_get_arg(effect, stat, opcode, 1);
-	udword var2 = do_get_arg(effect, stat, opcode, 2);
-	udword var3 = do_get_arg(effect, stat, opcode, 3);
-	udword var4 = do_get_arg(effect, stat, opcode, 4);
-	memsize (*func_ptr)(udword, udword, udword, udword, udword) = (memsize (*)(udword, udword, udword, udword, udword)) opcode->function;
+    udword var0 = do_get_arg(effect, stat, opcode, 0);
+    udword var1 = do_get_arg(effect, stat, opcode, 1);
+    udword var2 = do_get_arg(effect, stat, opcode, 2);
+    udword var3 = do_get_arg(effect, stat, opcode, 3);
+    udword var4 = do_get_arg(effect, stat, opcode, 4);
+    memsize (*func_ptr)(udword, udword, udword, udword, udword) = (memsize(*)(udword, udword, udword, udword, udword))opcode->function;
 
-	return func_ptr(var0, var1, var2, var3, var4);
+    return func_ptr(var0, var1, var2, var3, var4);
 }
-
 
 void partModifyTexture(psysPtr psys, trhandle tex, udword u0, udword v0, udword u1, udword v1);
 
-memsize wrap_partModifyTexture(Effect *effect, struct etgeffectstatic *stat ,etgfunctioncall *opcode)
+memsize wrap_partModifyTexture(Effect *effect, struct etgeffectstatic *stat, etgfunctioncall *opcode)
 {
-	udword var0 = do_get_arg(effect, stat, opcode, 0);
-	udword var1 = do_get_arg(effect, stat, opcode, 1);
-	udword var2 = do_get_arg(effect, stat, opcode, 2);
-	udword var3 = do_get_arg(effect, stat, opcode, 3);
-	udword var4 = do_get_arg(effect, stat, opcode, 4);
-	udword var5 = do_get_arg(effect, stat, opcode, 5);
-	memsize (*func_ptr)(udword, udword, udword, udword, udword, udword) = (memsize (*)(udword, udword, udword, udword, udword, udword)) opcode->function;
+    udword var0 = do_get_arg(effect, stat, opcode, 0);
+    udword var1 = do_get_arg(effect, stat, opcode, 1);
+    udword var2 = do_get_arg(effect, stat, opcode, 2);
+    udword var3 = do_get_arg(effect, stat, opcode, 3);
+    udword var4 = do_get_arg(effect, stat, opcode, 4);
+    udword var5 = do_get_arg(effect, stat, opcode, 5);
+    memsize (*func_ptr)(udword, udword, udword, udword, udword, udword) = (memsize(*)(udword, udword, udword, udword, udword, udword))opcode->function;
 
-	return func_ptr(var0, var1, var2, var3, var4, var5);
+    return func_ptr(var0, var1, var2, var3, var4, var5);
 }
 
 void partSetMeshdata(meshdata *m);
 
-memsize wrap_partSetMeshdata(Effect *effect, struct etgeffectstatic *stat ,etgfunctioncall *opcode)
+memsize wrap_partSetMeshdata(Effect *effect, struct etgeffectstatic *stat, etgfunctioncall *opcode)
 {
-	udword var0 = do_get_arg(effect, stat, opcode, 0);
-	memsize (*func_ptr)(udword) = (memsize (*)(udword)) opcode->function;
+    udword var0 = do_get_arg(effect, stat, opcode, 0);
+    memsize (*func_ptr)(udword) = (memsize(*)(udword))opcode->function;
 
-	return func_ptr(var0);
+    return func_ptr(var0);
 }
 
 void etgCreateCallbackSetup(Effect *effect, sdword codeOffset);
 
-memsize wrap_etgCreateCallbackSetup(Effect *effect, struct etgeffectstatic *stat ,etgfunctioncall *opcode)
+memsize wrap_etgCreateCallbackSetup(Effect *effect, struct etgeffectstatic *stat, etgfunctioncall *opcode)
 {
-	udword var0 = do_get_arg(effect, stat, opcode, 0);
-	memsize (*func_ptr)(Effect *, udword) = (memsize (*)(Effect *, udword)) opcode->function;
+    udword var0 = do_get_arg(effect, stat, opcode, 0);
+    memsize (*func_ptr)(Effect *, udword) = (memsize(*)(Effect *, udword))opcode->function;
 
-	return func_ptr(effect, var0);
+    return func_ptr(effect, var0);
 }
-
 
 memsize etgSpawnNewEffect(Effect *effect, etgeffectstatic *stat, sdword nParams, ...);
 
-udword wrap_etgSpawnNewEffect(Effect *effect, struct etgeffectstatic *stat ,etgfunctioncall *opcode)
+udword wrap_etgSpawnNewEffect(Effect *effect, struct etgeffectstatic *stat, etgfunctioncall *opcode)
 {
-	udword nParams     = opcode->nParameters;
-	memsize intParam[8] = {0,0,0,0,0,0,0,0};
+    udword nParams = opcode->nParameters;
+    memsize intParam[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-	for (udword i = 0; i < nParams; i++)
-	{
-		intParam[i] = get_arg(i);
-	}
+    for (udword i = 0; i < nParams; i++)
+    {
+        intParam[i] = get_arg(i);
+    }
 
     etgSpawnNewEffect(
         effect,
-        (etgeffectstatic *) intParam[0],
-        (sdword) intParam[1], // nParams
+        (etgeffectstatic *)intParam[0],
+        (sdword)intParam[1], // nParams
         intParam[2],
         intParam[3],
         intParam[4],
@@ -74,34 +72,31 @@ udword wrap_etgSpawnNewEffect(Effect *effect, struct etgeffectstatic *stat ,etgf
         intParam[6],
         intParam[7]);
 
-	return(etgFunctionSize(nParams));
+    return (etgFunctionSize(nParams));
 }
-
 
 void etgCreateEffects(Effect *effect, etgeffectstatic *stat, sdword number, sdword dist, sdword nParams, ...);
 
-udword wrap_etgCreateEffects(Effect *effect, struct etgeffectstatic *stat ,etgfunctioncall *opcode)
+udword wrap_etgCreateEffects(Effect *effect, struct etgeffectstatic *stat, etgfunctioncall *opcode)
 {
-	udword nParams     = opcode->nParameters;
-	memsize intParam[8] = {0,0,0,0,0,0,0,0};
+    udword nParams = opcode->nParameters;
+    memsize intParam[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-	for (udword i = 0; i < nParams; i++)
-	{
-		intParam[i] = get_arg(i);
-	}
+    for (udword i = 0; i < nParams; i++)
+    {
+        intParam[i] = get_arg(i);
+    }
 
     etgCreateEffects(
         effect,
-        (etgeffectstatic *) intParam[0],
-        (sdword) intParam[1],
-        (sdword) intParam[2],
-        (sdword) intParam[3], // nParams
+        (etgeffectstatic *)intParam[0],
+        (sdword)intParam[1],
+        (sdword)intParam[2],
+        (sdword)intParam[3], // nParams
         intParam[4],
         intParam[5],
         intParam[6],
         intParam[7]);
 
-	return(etgFunctionSize(nParams));
+    return (etgFunctionSize(nParams));
 }
-
-
