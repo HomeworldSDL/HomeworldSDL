125a5f6d1ab70061f3d473d1856cae91e2881e59
Thibault Lemaire <thibault.lemaire@protonmail.com>
2019-03-12 14:26:51 +0100

    Fix wrap types
    So it's up to the wrapped function to cast its parameters upon calling,
    and therefore pointers are not truncated.
    
    This is actually the final step for the 64b compatibility of the generic
    code.
    
    It is now officially **working** !

--- src/Game/wrap_types.h
+++ src/Game/wrap_types.h
@@ -1,6 +1,6 @@
-#define _EVT_Void                    udword
-#define _EVT_Int                     udword
+#define _EVT_Void                    memsize
+#define _EVT_Int                     memsize
 #define _EVT_Float                   real32
-#define _EVT_RGB                     udword
-#define _EVT_RGBA                    udword
-#define _EVT_ConstLabel		     udword
+#define _EVT_RGB                     memsize
+#define _EVT_RGBA                    memsize
+#define _EVT_ConstLabel              memsize
