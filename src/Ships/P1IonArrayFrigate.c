31c100441648cbae4c836434ccc4a2fe3b8fccd2
Kaji Tetsushi <kajitetsushi@outlook.com>
2020-12-03 00:26:57 +0800

    feat(ships): add primitive cloak
    P1IonArrayFrigate
    
    To imitate Homeworld:Cataclysm special ability. This change doesn't take
    energy into consideration yet, but it's definitely a step forward in the
    right direction.
    
    [#10]

--- src/Ships/P1IonArrayFrigate.c
+++ src/Ships/P1IonArrayFrigate.c
@@ -8,17 +8,29 @@
 #include "P1IonArrayFrigate.h"
 
 #include "Attack.h"
+#include "Battle.h"
 #include "DefaultShip.h"
+#include "Gun.h"
+#include "SoundEvent.h"
+#include "Universe.h"
 
 typedef struct
 {
     udword dummy;
+    real32 CloakingStatus; // Gradient value for cloaking and decloaking state.
+    bool CloakLowWarning;  // Indicates whether the ship's cloak mana is low.
+    bool ReCloak;          // Indicates whether the ship needs to cloak again.
+    real32 ReCloakTime;    // Timestamp (in seconds) against game time elapsed during which the ship must cloak again.
 } P1IonArrayFrigateSpec;
 
 typedef struct
 {
     real32 frigateGunRange[NUM_TACTICS_TYPES];
     real32 frigateTooCloseRange[NUM_TACTICS_TYPES];
+    real32 CloakingTime;      // Cloaking delay (in seconds).
+    real32 DeCloakingTime;    // Decloaking delay (in seconds).
+    real32 VisibleState;      // Threshold at which the ship becomes "invisible" to sensors, ships and everything else.
+    real32 battleReCloakTime; // Recloaking delay (in seconds) after the ship has fired.
 } P1IonArrayFrigateStatics;
 
 P1IonArrayFrigateStatics P1IonArrayFrigateStatic;
@@ -35,6 +47,16 @@ void P1IonArrayFrigateStaticInit(char *directory,char *filename,struct ShipStati
         frigstat->frigateGunRange[i] = statinfo->bulletRange[i];
         frigstat->frigateTooCloseRange[i] = statinfo->minBulletRange[i] * 0.9f;
     }
+
+    frigstat->CloakingTime = 1 / frigstat->CloakingTime;
+}
+
+void P1IonArrayFrigateInit(Ship *ship)
+{
+    P1IonArrayFrigateSpec *spec = (P1IonArrayFrigateSpec *)ship->ShipSpecifics;
+    spec->CloakingStatus = 1.0f; //ship isn't cloaked
+    spec->CloakLowWarning = FALSE;
+    spec->ReCloak = FALSE;
 }
 
 void P1IonArrayFrigateAttack(Ship *ship,SpaceObjRotImpTarg *target,real32 maxdist)
@@ -57,20 +79,112 @@ void P1IonArrayFrigateAttackPassive(Ship *ship,Ship *target,bool rotate)
     }
 }
 
+void P1IonArrayFrigateSpecialActivate(Ship *ship)
+{
+    P1IonArrayFrigateSpec *spec = (P1IonArrayFrigateSpec *)ship->ShipSpecifics;
+    P1IonArrayFrigateStatics *stat = (P1IonArrayFrigateStatics *)((ShipStaticInfo *)(ship->staticinfo))->custstatinfo;
+
+    spec->ReCloak = FALSE;
+
+    if (bitTest(ship->flags, SOF_Cloaked))
+    {
+        bitSet(ship->flags, SOF_DeCloaking); //begin DeCloaking Process;
+        SpawnCloakingEffect(ship, etgSpecialPurposeEffectTable[EGT_CLOAK_OFF]);
+        soundEvent(ship, Ship_CloakOff);
+    }
+    else
+    {
+        bitSet(ship->flags, SOF_Cloaking); //begin Cloaking Process
+        SpawnCloakingEffect(ship, etgSpecialPurposeEffectTable[EGT_CLOAK_ON]);
+        soundEvent(ship, Ship_CloakOn);
+        if (battleCanChatterAtThisTime(BCE_CloakingOn, ship))
+        {
+            battleChatterAttempt(SOUND_EVENT_DEFAULT, BCE_CloakingOn, ship, SOUND_EVENT_DEFAULT);
+        }
+    }
+
+    return TRUE;
+}
+
+void P1IonArrayFrigateHouseKeep(Ship *ship)
+{
+    P1IonArrayFrigateSpec *spec = (P1IonArrayFrigateSpec *)ship->ShipSpecifics;
+    P1IonArrayFrigateStatics *stat = (P1IonArrayFrigateStatics *)((ShipStaticInfo *)(ship->staticinfo))->custstatinfo;
+
+    if (bitTest(ship->flags, SOF_Cloaking)) //ship is cloaking
+    {
+        //decrement Cloaking Status counter based on time elapsed
+        /***** calculated the inverse of CloakingTime in P1IonArrayFrigateStaticInit and multiply instead of divide *****/
+        spec->CloakingStatus -= universe.phystimeelapsed * stat->CloakingTime;
+        if (spec->CloakingStatus <= stat->VisibleState)
+        {                                     //if ship is now past the vissible threshold
+            bitSet(ship->flags, SOF_Cloaked); //make it invisible to everything
+            if (spec->CloakingStatus <= 0.0f) //if it is completly invisible stop decloaking
+            {
+                spec->CloakingStatus = 0.0f;         //reset to 0.0
+                bitClear(ship->flags, SOF_Cloaking); //stop ship from 'cloaking'
+                //Since ship is cloaked, must remove it from being targeted
+                //Note:  This probably will not let a person attack their own cloaked ships...
+
+                shipHasJustCloaked(ship);
+            }
+        }
+    }
+    else if (bitTest(ship->flags, SOF_DeCloaking)) //Ship is decloaking
+    {
+        //Increment Cloaking Status counter based on time elapsed
+        /***** calculated the inverse of CloakingTime in P1IonArrayFrigateStaticInit and multiply instead of divide *****/
+        spec->CloakingStatus += universe.phystimeelapsed * stat->CloakingTime;
+        if (spec->CloakingStatus >= stat->VisibleState)
+        { //ship is 'visible' since it is past visible threshold
+            if (ship->flags & SOF_Cloaked)
+            {
+                bitClear(ship->flags, SOF_Cloaked);
+                ship->shipDeCloakTime = universe.totaltimeelapsed;
+            }
+            if (spec->CloakingStatus >= 1.0f) //done decloaking
+            {
+                spec->CloakingStatus = 1.0f;
+                bitClear(ship->flags, SOF_DeCloaking);
+                spec->CloakLowWarning = FALSE; //reset flag
+                if (spec->ReCloak == FALSE)
+                { //only play speech when really decloaking for good
+                    if (battleCanChatterAtThisTime(BCE_Decloaking, ship))
+                    {
+                        battleChatterAttempt(SOUND_EVENT_DEFAULT, BCE_Decloaking, ship, SOUND_EVENT_DEFAULT);
+                    }
+                }
+            }
+        }
+    }
+
+    // TODO: @kajitetsushi Consume energy for running cloak. See the cloak generator for ideas.
+
+    if (spec->ReCloak == TRUE)
+    {
+        if (universe.totaltimeelapsed > spec->ReCloakTime)
+        {
+            spec->ReCloak = FALSE;
+            bitSet(ship->flags, SOF_Cloaking);
+            soundEvent(ship, Ship_CloakOn);
+        }
+    }
+}
+
 CustShipHeader P1IonArrayFrigateHeader =
 {
     P1IonArrayFrigate,
     sizeof(P1IonArrayFrigateSpec),
     P1IonArrayFrigateStaticInit,
     NULL,
-    NULL,
+    P1IonArrayFrigateInit,
     NULL,
     P1IonArrayFrigateAttack,
     DefaultShipFire,
     P1IonArrayFrigateAttackPassive,
+    P1IonArrayFrigateSpecialActivate,
     NULL,
-    NULL,
-    NULL,
+    P1IonArrayFrigateHouseKeep,
     NULL,
     NULL,
     NULL,
