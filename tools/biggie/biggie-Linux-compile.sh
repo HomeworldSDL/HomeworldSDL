65c60bb81b3684ca567cbed852af42bf846c5e9a
Thibault Lemaire <thibault.lemaire@protonmail.com>
2021-09-18 14:07:13 +0200

    Fix biggie build after sdl2-config name changed

--- tools/biggie/biggie-Linux-compile.sh
+++ tools/biggie/biggie-Linux-compile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cc -o biggie -Wall -O2 -D_LINUX_FIX_ME -I../../src/ThirdParty/CRC \
         -I../../src/SDL \
-	-I../../src/ThirdParty/LZSS `sdl-config --cflags` \
+	-I../../src/ThirdParty/LZSS `sdl2-config --cflags` \
 	main.c options.c ../../src/Game/BigFile.c \
 	../../Linux/src/ThirdParty/CRC/libhw_CRC.a \
-	../../Linux/src/ThirdParty/LZSS/libhw_LZSS.a `sdl-config --libs`
+	../../Linux/src/ThirdParty/LZSS/libhw_LZSS.a `sdl2-config --libs`
